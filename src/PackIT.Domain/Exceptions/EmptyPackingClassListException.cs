0f55d5ed7e6bf9f1a81d967a2879cb443cf3168c
amyard <maksymturenko@gmail.com>
2021-11-27 18:31:06 +0900

    create entity and some custom error handling

---
+++ src/PackIT.Domain/Exceptions/EmptyPackingClassListException.cs
@@ -0,0 +1,11 @@
+﻿using PackIT.Shared.Abstractions.Exceptions;
+
+namespace PackIT.Domain.Exceptions
+{
+    public class EmptyPackingClassListException : PackItException
+    {
+        public EmptyPackingClassListException() : base("packing list name cannot be empty")
+        {
+        }
+    }
+}
---
+++ src/PackIT.Domain/ValueObjects/PackingListName.cs
@@ -0,0 +1,20 @@
+﻿using PackIT.Domain.Exceptions;
+
+namespace PackIT.Domain.ValueObjects
+{
+    public record PackingListName
+    {
+        public string Value { get; }
+
+        public PackingListName(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new EmptyPackingClassListException();
+                
+            Value = value;
+        }
+
+        public static implicit operator string(PackingListName name) => name.Value;
+        public static implicit operator PackingListName(string name) => new(name);
+    }
+}
