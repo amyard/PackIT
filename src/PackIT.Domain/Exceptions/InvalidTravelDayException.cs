fb4a33785cb35f5a0a6fc5f286d5d7ac24049d90
amyard <maksymturenko@gmail.com>
2021-12-24 05:27:11 +0900

    add factory

---
+++ src/PackIT.Domain/Exceptions/InvalidTravelDayException.cs
@@ -0,0 +1,14 @@
+﻿using PackIT.Shared.Abstractions.Exceptions;
+
+namespace PackIT.Domain.Exceptions
+{
+    public class InvalidTravelDayException : PackItException
+    {
+        public ushort Days { get; }
+
+        public InvalidTravelDayException(ushort days) : base($"Value 'days' is invalid travel days.")
+        {
+            Days = days;
+        }
+    }
+}
