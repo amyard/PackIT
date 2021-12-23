3785d09cc48c9aae889b5048380c77b65e9eb898
amyard <maksymturenko@gmail.com>
2021-12-24 07:27:39 +0900

    register dependencies

---
+++ src/PackIT.Application/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PackIT.Domain.Factories;
+using PackIT.Domain.Policies;
+using PackIT.Shared;
+
+namespace PackIT.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // register all command from Shared APP
+            services.AddCommands();  
+            
+            services.AddSingleton<IPackingListFactory, PackingListFactory>();
+            
+            // register all Policies
+            services.Scan(b => b.FromAssemblies(typeof(IPackingItemsPolicy).Assembly)
+                .AddClasses(c => c.AssignableTo<IPackingItemsPolicy>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+            
+            return services;
+        }
+    }
+}
